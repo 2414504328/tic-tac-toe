033b53c75211cf42e61c625e2a163658006f6467
2414504328 <2414504328@mujonline.edu.in>
2025-05-25 17:51:00 +0530

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include "raylib.h"
+
+struct ScreenSize
+{
+	int width, hight;
+};
+
+ScreenSize screenSize = { 600, 600 };
+char tttBoard[3][3] = { {' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '} };
+int fountSize = 212;
+int xOffset = 31;
+
+void DrawText(const char* text, int x, int y);
+
+int main ()
+{
+	// Tell the window to use vsync and work on high DPI displays
+	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+
+	// Create the window and OpenGL context
+	InitWindow(screenSize.width, screenSize.hight, "Tic Tac Toe");
+	
+	// game loop
+	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
+	{
+		// drawing
+		BeginDrawing();
+
+		// Setup the back buffer for drawing (clear color and depth buffers)
+		ClearBackground(WHITE);
+
+		DrawRectangle(194, 0, 9, screenSize.hight, BLACK);
+		DrawRectangle(397, 0, 9, screenSize.hight, BLACK);
+		DrawRectangle(0, 194, screenSize.width, 9, BLACK);
+		DrawRectangle(0, 397, screenSize.width, 9, BLACK);
+
+		// draw some text using the default font
+		DrawText("X", 0, 0);
+		DrawText("O", 1, 1);
+		
+		// end the frame and get ready for the next one  (display frame, poll input, etc...)
+		EndDrawing();
+	}
+
+	// destroy the window and cleanup the OpenGL context
+	CloseWindow();
+	return 0;
+}
+
+void DrawText(const char* text, int x, int y)
+{
+	DrawText(text, x * 203 + xOffset, y * 203, fountSize, BLACK);
+}
