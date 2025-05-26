24e8b2dfbff9367027cbd2475eaed922d8e6b438
2414504328 <2414504328@mujonline.edu.in>
2025-05-26 21:28:04 +0530

    added player moves and turn

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,14 @@ struct ScreenSize
 };
 
 short tttBoard[3][3] = { {0, 0, 0}, {0, 0, 0}, {0, 0, 0} };
+bool isOTurn = false;
 
 const ScreenSize screenSize = { 600, 640 };
 const int bottomBarHight = 40;
 const int xOffset = 31;
 const int fountSize = 212;
 const char* playerChar[3] = {" ", "X", "O"};
+
 
 void Reset();
 void Update();
@@ -60,35 +62,60 @@ void Reset() {
 			tttBoard[i][j] = 0;
 		}
 	}
+	isOTurn = false;
 }
 
 void Update() {
 	bool isMouseClicked = IsMouseButtonPressed(0);
-	if (isMouseClicked) {
-		int mousePosX = GetMouseX();
-		int mousePosY = GetMouseY();
-		//cout << "Mouse Pos: X=" << mousePosX << " Y=" << mousePosY << endl;
-		if (mousePosX >= 0 && mousePosX <= 200) { // A colloum clicked
-			cout << "A colloum clicked" << endl;
-		}else if (mousePosX >= 200 && mousePosX <= 400) { // B colloum clicked
-			cout << "B colloum clicked" << endl;
-		}
-		else /*if (mousePosX >= 400 && mousePosX <= 600)*/ { // C colloum clicked
-			cout << "C colloum clicked" << endl;
-		}
-		if (mousePosY >= 0 && mousePosY <= 200) { // 1st Row clicked
-			cout << "1st Row clicked" << endl;
-		}
-		else if (mousePosY >= 200 && mousePosY <= 400) { // 2nd Row clicked
-			cout << "2nd Row clicked" << endl;
-		}
-		else if (mousePosY >= 400 && mousePosY <= 600) { // 3rd Row clicked
-			cout << "3rd Row clicked" << endl;
-		}
-		else { // 4th Row clicked
-			cout << "4th Row clicked" << endl;
-		}
+
+	if (!isMouseClicked) {
+		return;
 	}
+
+	int mousePosX = GetMouseX();
+	int mousePosY = GetMouseY();
+	short coloum = -1;
+	short row = -1;
+	bool isBottomBarClicked = false;
+	//cout << "Mouse Pos: X=" << mousePosX << " Y=" << mousePosY << endl;
+	if (mousePosX >= 0 && mousePosX <= 200) { // A colloum clicked
+		cout << "A colloum clicked" << endl;
+		coloum = 0;
+	}
+	else if (mousePosX >= 200 && mousePosX <= 400) { // B colloum clicked
+		cout << "B colloum clicked" << endl;
+		coloum = 1;
+	}
+	else /*if (mousePosX >= 400 && mousePosX <= 600)*/ { // C colloum clicked
+		cout << "C colloum clicked" << endl;
+		coloum = 2;
+	}
+	if (mousePosY >= 0 && mousePosY <= 200) { // 1st Row clicked
+		cout << "1st Row clicked" << endl;
+		row = 0;
+	}
+	else if (mousePosY >= 200 && mousePosY <= 400) { // 2nd Row clicked
+		cout << "2nd Row clicked" << endl;
+		row = 1;
+	}
+	else if (mousePosY >= 400 && mousePosY <= 600) { // 3rd Row clicked
+		cout << "3rd Row clicked" << endl;
+		row = 2;
+	}
+	else { // 4th Row clicked
+		cout << "4th Row clicked" << endl;
+		row = 3;
+		isBottomBarClicked = true;
+	}
+
+	if (isBottomBarClicked) {
+		return;
+	}
+
+
+
+	tttBoard[row][coloum] = 1 + isOTurn;
+	isOTurn = !isOTurn;
 }
 
 void DrawGame() {
@@ -108,7 +135,7 @@ void DrawGame() {
 	}
 }
 
-void DrawText(const char* text, int x, int y)
+void DrawText(const char* text, int rowIndex, int coloumIndex)
 {
-	DrawText(text, x * 203 + xOffset, y * 203, fountSize, BLACK);
+	DrawText(text, coloumIndex * 203 + xOffset, rowIndex * 203, fountSize, BLACK);
 }
